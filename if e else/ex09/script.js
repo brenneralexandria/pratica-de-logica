678d7daa37abd7860644715bcfb1d82dd39da9d0
brenneralexandria <obrenneralexandria@gmail.com>
2023-02-10 17:27:46 -0300

    ex09

---
+++ if e else/ex09/script.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("Digite um número"));
+
+if(num >= 1) {
+    alert('O número é positivo');
+}else if(num < 0) {
+    alert('O número é negativo');
+}else {
+    alert('O número é 0');
+}
