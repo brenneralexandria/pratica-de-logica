c8597f1fe2bd025328e41949021d3b1337e8db25
brenneralexandria <obrenneralexandria@gmail.com>
2023-02-10 16:10:16 -0300

    ex03

---
+++ if e else/ex03/script.js
@@ -0,0 +1,9 @@
+let idade = parseInt(prompt("Digite a sua idade"));
+
+let deMaior = 18;
+
+if(idade >= deMaior) {
+    alert('Liberado, você pode entrar na boate!');
+}else {
+    alert('Você não pode entrar na boate ainda, complete seus 18 anos!');
+}
