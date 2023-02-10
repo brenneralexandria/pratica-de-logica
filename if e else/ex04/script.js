1c179a106a5f0ab291e1cdfd7b73117e761b8d94
brenneralexandria <obrenneralexandria@gmail.com>
2023-02-10 16:13:38 -0300

    ex04

---
+++ if e else/ex04/script.js
@@ -0,0 +1,6 @@
+let anoAtual = parseInt(prompt("Digite o ano atual"));
+let anoNascimento = parseInt(prompt("Digite o ano de seu nascimento"));
+
+let soma = anoAtual - anoNascimento;
+
+alert('Sua idade aproximada é ' + soma );
