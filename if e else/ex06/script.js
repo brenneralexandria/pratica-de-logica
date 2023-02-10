4c0268731e8ffbd79b131cc8a8c5e357d5abbcd7
brenneralexandria <obrenneralexandria@gmail.com>
2023-02-10 16:55:18 -0300

    ex06

---
+++ if e else/ex06/script.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt('Digite o primeiro número'));
+let num2 = parseInt(prompt('Digite o segundo número'));
+
+if(num1 === num2 ) {
+    alert('Os números são iguais');
+}else {
+    alert('Os números são diferentes');
+}
